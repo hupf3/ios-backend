fa8dc5e38ebe9eb4e404e6670535aaa404dc4346
hupf <945554668@qq.com>
2020-12-28 20:20:06 +0800

    update

--- models/user.go
+++ models/user.go
@@ -7,20 +7,3 @@ type User struct {
 	Password string `json:"password"`
 	// Avatar   string `json:"avatar"`
 }
-
-// // CreateUserTable 创建用户表
-// func CreateUserTable() {
-// 	sql := `CREATE TABLE IF NOT EXISTS users(
-// 		user_id INT NOT NULL AUTO_INCREMENT,
-// 		user_name VARCHAR(32) UNIQUE,
-// 		password VARCHAR(32),
-// 		bio VARCHAR(128) DEFAULT '',
-// 		PRIMARY KEY (user_id)
-// 		)ENGINE=InnoDB DEFAULT CHARSET=utf8;`
-
-// 	// 执行建表
-// 	if _, err := db.Exec(sql); err != nil {
-// 		fmt.Println("Create userTable failed!", err)
-// 		return
-// 	}
-// }
