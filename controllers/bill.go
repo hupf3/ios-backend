291f8c22dc11a0d16fa7dffd25a1d5333ddac174
hupf <945554668@qq.com>
2020-12-28 21:12:45 +0800

    update

--- controllers/bill.go
+++ controllers/bill.go
@@ -1,12 +1,33 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/hupf3/ios-backend/models"
 )
 
 // AddBill 增加账单
 func AddBill(c *gin.Context) {
+	var b models.Bill
 
+	if err := c.BindJSON(&b); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "failed",
+			"error":  "binding error",
+		})
+		return
+	}
+
+	if err := models.InsertBill(b); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "failed",
+			"error":  err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "succeed"})
 }
 
 // DeleteBillByBillID 删除账单
@@ -16,8 +37,8 @@ func DeleteBillByBillID(c *gin.Context) {
 
 // GetBillByBillID 获取账单
 func GetBillByBillID(c *gin.Context) {
-	billID := c.Param("billID")
-	println(billID)
+	// billID := c.Param("billID")
+	// println(billID)
 }
 
 // UpdateBillByBillID 修改账单
--- models/bill.go
+++ models/bill.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 // Bill 账单
 type Bill struct {
 	BillID   int    `json:"bill_id"`
@@ -7,4 +12,16 @@ type Bill struct {
 	Money    string `json:"money"`
 	BillTime string `json:"bill_time"`
 	Tag      string `json:"tag"`
+}
+
+// InsertBill 插入账单信息
+func InsertBill(b Bill) error {
+	_, err := db.Exec("insert INTO bill(bill_id, user_id, money, tag) values(?,?)", b.BillID, b.UserID, b.Money, b.Tag)
+
+	if err != nil {
+		fmt.Printf("Insert bill failed, err:%v", err)
+		return errors.New("Bill exists")
+	}
+
+	return nil
 }
