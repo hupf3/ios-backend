1fe7852676dc2efba532a8fdf4c5b6116bb3bade
hupf <945554668@qq.com>
2020-12-28 20:50:23 +0800

    update

--- controllers/bill.go
+++ controllers/bill.go
@@ -1,1 +1,26 @@
 package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// AddBill 增加账单
+func AddBill(c *gin.Context) {
+
+}
+
+// DeleteBillByBillID 删除账单
+func DeleteBillByBillID(c *gin.Context) {
+
+}
+
+// GetBillByBillID 获取账单
+func GetBillByBillID(c *gin.Context) {
+	billID := c.Param("billID")
+	println(billID)
+}
+
+// UpdateBillByBillID 修改账单
+func UpdateBillByBillID(c *gin.Context) {
+
+}
--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"github.com/hupf3/ios-backend/models"
-	// "github.com/hupf3/ios-backend/route"
+	"github.com/hupf3/ios-backend/routes"
 	// "github.com/hupf3/ios-backend/service"
+	// "github.com/hupf3/ios-backend/controllers"
 )
 
 func main() {
+	// 连接数据库
 	models.Connect()
+
+	// 设置路由
+	router := routes.SetRouter()
+	router.Run(":8081")
 
 	// 断开数据库连接
 	// defer db.Close()
