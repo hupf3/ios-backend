49117e8c49686bda7aa0fb54e1bf265026779d86
Wenxu Zhao <395777074@qq.com>
2020-12-29 15:12:16 +0800

    update database.go

--- models/database.go
+++ models/database.go
@@ -11,8 +11,8 @@ import (
 // 数据库的信息
 const (
 	USERNAME = "root"      // 用户名
-	PASSWORD = "zwx20000106"  // 密码
-	DATABASE = "test"     // 数据库名字
+	PASSWORD = "huq35999"  // 密码
+	DATABASE = "mosad"     // 数据库名字
 	PORT     = "3306"      // 端口
 	IP       = "127.0.0.1" // IP地址
 )
